35f8c740f7dbcf7a0ffb849e0eb2165d27f65964
Kshitij Mudgal <iamkshitij@live.com>
2022-11-13 20:33:10 +0530

    [ADDED] new model class to the project

--- main.go
+++ main.go
@@ -1,16 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/iamkshitij/pluralsight-go/models"
+)
 
 func main() {
-	// d1 := complex(3, 4)
-	// d2 := complex(7, 6)
-
-	// var d3 = d1 + d2
-
-	// fmt.Println(d3)
-
-	var firstName *string = new(string)
-	*firstName = "Arthur"
-	fmt.Println(firstName, *firstName)
+	u := models.User{
+		ID:        2,
+		FirstName: "Alex",
+		LastName:  "Hales",
+	}
+	fmt.Println(u)
 }
